e5cb6ffc2529d7eae4f5b9f9d77fb9e7c7f220fa
JOHN YU <yjunji@l-b-v239-03.du.deakin.edu.au>
2018-04-18 20:01:32 +1000

    Project

---
+++ SIT305GroupProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SIT305GroupProject
+//
+//  Created by JOHN YU on 18/4/18.
+//  Copyright © 2018 SIT305. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
